05f5d29f78dbce395a79a064267ed5d0da02d0ec
Lindsey Fares <lrfares1996@gmail.com>
2020-11-09 17:12:26 -0600

    Changed Volume to Area

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
 // Initilaization
-function multiply(x, y, z) { return (x * y * z) };
+function multiply(x, y) { return x * y }
 
 window.addEventListener('load', (event) => {
   if (localStorage.getItem('user')) {
-    document.querySelector('user').value = localStorage.getItem('user')
+    document.querySelector('user').value = localStorage.getItem('user');
   };
   if (localStorage.getItem('num1')) {
-    document.querySelector('#height').value = parseInt(localStorage.num1)
+    document.querySelector('#height').value = parseInt(localStorage.num1);
   };
   if (localStorage.getItem('num2')) {
-    document.querySelector('#length').value = parseInt(localStorage.num2)
+    document.querySelector('#length').value = parseInt(localStorage.num2);
   };
-  if (localStorage.getItem('num3')) {
-    document.querySelector('#width').value = parseInt(localStorage.num3)
-  };
-})
+});
 // Initialization END
 
 // Event Listeners
@@ -23,16 +20,14 @@ document.querySelector('#volumeBtn').addEventListener('click', () => {
   const name = parseInt(document.querySelector('#user').value)
   const x = parseInt(document.querySelector('#num1').value)
   const y = parseInt(document.querySelector('#num2').value)
-  const z = parseInt(document.querySelector('#num3').value)
   const addClick = counterStart + 1
-  const response = `${name}, your volume is ${multiply(x, y, z)}. This script has been run a total of ${addClick} times.`
+  const response = `${name}, your volume is ${multiply(x, y)}. This script has been run a total of ${addClick} times.`
   document.querySelector('#result').innerHTML = response
 
   localStorage.setItem('clickCount', addClick)
   localStorage.setItem('user', name)
   localStorage.setItem('num1', x)
   localStorage.setItem('num2', y)
-  localStorage.setItem('num3', z)
 })
 
 document.querySelector('#clearBtn').addEventListener('click', () => {
@@ -40,5 +35,4 @@ document.querySelector('#clearBtn').addEventListener('click', () => {
   localStorage.removeItem('user')
   localStorage.removeItem('num1')
   localStorage.removeItem('num2')
-  localStorage.removeItem('num3')
 })
